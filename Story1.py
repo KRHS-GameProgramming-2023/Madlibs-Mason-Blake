cb3a87a933cfa2163b8d50ad042cd3317e663f7f
Blake Grieco <blake.grieco@kearsareg.org>
2022-11-03 07:48:32 -0400

    story1

--- Story1.py
+++ Story1.py
@@ -1,4 +1,4 @@
-Sfrom Getters import *
+from Getters import *
 
 def Story1(debug = False):
     if debug: print("Story1 Function") 
@@ -6,11 +6,24 @@ def Story1(debug = False):
     print("\n")
     friendName1 = getWord("Enter a name ", debug)
     sport1 = getSport("Enter a sport ", debug)
+    emerancy1= getWord("enter a emergencey",debug)
+    hospital1= getWord("enter a hospital",debug)
+    resteraunt= getWord("enter a resteraunt",debug)
+    food= getWord("enter a food",debug)
+    movie= getWord("enter a movie",debug
+    
     
     out = "\n"
     out += "\n"
     out+= "One day me and my friend " + friendName1
     out+= " Were out playing " + sport1
+    out+= " and they had a  " + emerancy1
+    out+= " they went to " + hospital1
+    out+= " after the check up they went to " + resteraunt 
+    out+= " they got home and made some " + food 
+    out+= " they got there food then watched " + movie 
+    out+= " after the movie they went to bed "
+    
     
     
     
